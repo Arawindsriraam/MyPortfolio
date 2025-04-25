9e111cb306796f38e0162c69ad012c90fe35407e
Arawindsriram <arawindsriram@themajesticpeople.com>
2025-04-25 18:58:34 +0530

    Updated Vite config and index.html for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/MyPortfolio" : "/", // 👈 base is conditional
+  base: mode === "production" ? "/MyPortfolio/" : "/", // 👈 base is conditional
   server: {
     host: "::",
     port: 8080,
